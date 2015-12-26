900a17a1065964bc60f2f13635457379fd9d29ab
stebifan <stefan@tro-mail.de>
2015-12-26 13:36:37 +0100

    Update dhcp-update.sh

--- dhcp-update.sh
+++ dhcp-update.sh
@@ -3,18 +3,17 @@ cd /opt/freifunk/static-dhcp
 git remote update
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
-data=`cat ./static.conf | sort | uniq -d`
+data=`cat ./static.conf.template | sort | uniq -d`
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
     exit 0
 else
     echo "Need to pull"
-    git checkout master
     git pull
 fi
 if [ ${#data} -gt 1 ]; then
-        git checkout fallback
         exit 1;
 else
+        cp ./static.conf.template ./static.conf
         /etc/init.d/isc-dhcp-server restart
 fi
